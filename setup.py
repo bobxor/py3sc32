990dcfe5a871ade0e6875c506413baea49d644fc
Rob McManus <rmcmanus@nextflex.us>
2020-10-10 20:01:11 -0700

    Correcting setup.py install.

--- setup.py
+++ setup.py
@@ -1,9 +1,9 @@
-import setuptools
+from setuptools import setup
 
 with open("README.md", "r") as fh:
     long_description = fh.read()
 
-setuptools.setup(
+setup(
     name="py3ssc32", 
     version="0.0.1",
     author="Robert McManus",
@@ -12,7 +12,9 @@ setuptools.setup(
     long_description=long_description,
     long_description_content_type="text/markdown",
     url="https://github.com/bobxor/py3ssc32",
-    packages=setuptools.find_packages(),
+#    packages=setuptools.find_packages(),
+	packages=['py3ssc32'],
+	zip_safe=False,
     classifiers=[
         "Programming Language :: Python :: 3",
         "License :: OSI Approved :: MIT License",
