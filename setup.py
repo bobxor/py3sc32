2f4b661b85f847389c840606f95b1deb6b630e22
bobxor <robert.mcmanus@gmail.com>
2020-10-10 18:58:13 -0700

    Update setup.py
    Updating project name.

--- setup.py
+++ setup.py
@@ -4,14 +4,14 @@ with open("README.md", "r") as fh:
     long_description = fh.read()
 
 setuptools.setup(
-    name="py3sc32", 
+    name="py3ssc32", 
     version="0.0.1",
     author="Robert McManus",
     author_email="robert.mcmanus@gmail.com",
     description="A Python 3 version of the PySSC32 ",
     long_description=long_description,
     long_description_content_type="text/markdown",
-    url="https://github.com/bobxor/py3sc32",
+    url="https://github.com/bobxor/py3ssc32",
     packages=setuptools.find_packages(),
     classifiers=[
         "Programming Language :: Python :: 3",
